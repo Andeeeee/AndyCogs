b3ce82f069f6f262fb64afe73449c2fe1de7c3e5
AndyButAnnoying <76832778+AndyButAnnoying@users.noreply.github.com>
2021-01-05 21:38:57 -0800

    one app per member, clear app on result

--- applications/application.py
+++ applications/application.py
@@ -8,7 +8,7 @@ import asyncio
 class Applications(commands.Cog):
     def __init__(self, bot):
         self.bot = bot 
-        self.config = Config.get_conf(self, identifier=160805014090190130501014, force_registration=True)
+        self.config = Config.get_conf(self, identifier=160805014090190130501014, force_registration=True) #e
 
         default_guild = {
             "questions" : [],
