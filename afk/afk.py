aede95111f35c20fcfa8182977ced8de05ca3ae9
AndyButAnnoying <76832778+AndyButAnnoying@users.noreply.github.com>
2021-01-07 14:26:20 -0800

    edit nicknames

--- afk/afk.py
+++ afk/afk.py
@@ -37,8 +37,10 @@ class Afk(commands.Cog):
         await ctx.send("I have set you as afk. People who ping you will now receive a message")
 
         if ctx.channel.permissions_for(ctx.me).manage_nicknames:
-            if not ctx.me.top_role.position <= ctx.author.top_role.position: 
+            try:
                 await ctx.author.edit(nick=f"[afk] {ctx.author.display_name}")
+            except discord.errors.Forbidden:
+                pass
     
     @afk.command(name="off")
     async def afk_off(self, ctx):
