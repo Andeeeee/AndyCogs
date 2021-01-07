5b0404733cb49304371ac3e67478e7bbade07a37
AndyButAnnoying <76832778+AndyButAnnoying@users.noreply.github.com>
2021-01-07 13:41:45 -0800

    Add files via upload

--- afk/afk.py
+++ afk/afk.py
@@ -83,7 +83,7 @@ class Afk(commands.Cog):
         if len(mentions) == 0:
             return 
 
-        for mention in mentions:
+        for str(mention) in mentions:
             userid = int(mention.lstrip("<@!").lstrip("<@").rstrip(">"))
             user = await guild.get_member(userid)
             if not user:
