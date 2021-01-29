073966ee168260e92c6fa2bade3f959a42b16555
Andy <76832778+Andeeeee@users.noreply.github.com>
2021-01-28 22:51:47 -0800

    prints

--- giveaways/giveaway.py
+++ giveaways/giveaway.py
@@ -665,6 +665,8 @@ class Giveaways(commands.Cog):
                     if not channel:
                         continue
                     m = self.cache.get(messageid)
+                    if m:
+                        return await ctx.send("Cache")
                     if not m:
                         try:
                             m = await channel.fetch_message(int(messageid))
