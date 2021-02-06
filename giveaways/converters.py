220de7cb7e78c0fbc05b12c8460bf93652850a3b
Andy <76832778+Andeeeee@users.noreply.github.com>
2021-02-06 14:00:10 -0800

    .

--- giveaways/converters.py
+++ giveaways/converters.py
@@ -48,4 +48,4 @@ class FuzzyRole(RoleConverter):
             raise BadArgument(f'Role "{argument}" not found.' if self.response else None)
 
         sorted_result = sorted(result, key=lambda r: r[1], reverse=True)
-        return sorted_result
+        return sorted_result[0]
