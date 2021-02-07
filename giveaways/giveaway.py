5ab8b2454a620f3b837f0711bd4d4b2f578b6a7e
Andy <76832778+Andeeeee@users.noreply.github.com>
2021-02-07 10:43:15 -0800

    fixes

--- giveaways/giveaway.py
+++ giveaways/giveaway.py
@@ -509,7 +509,6 @@ class Giveaways(commands.Cog):
     ):
         """Start a giveaway in your server. Flags and Arguments are explained with .giveaway help
         """
-        return await ctx.send(role)
         title = title.split("--")
         title = title[0]
         flags = ctx.message.content
