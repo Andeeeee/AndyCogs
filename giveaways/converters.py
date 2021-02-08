f88eaea2a3601d329592fd91b11782f2de39701c
Andy <76832778+Andeeeee@users.noreply.github.com>
2021-02-08 11:08:11 -0800

    message link parsing

--- giveaways/converters.py
+++ giveaways/converters.py
@@ -1,21 +1,12 @@
-#Originally from Phen-Cogs https://github.com/phenom4n4n/phen-cogs/blob/master/lock/converters.py
-
 from typing import Union
-
 import discord
-from unidecode import unidecode
 from rapidfuzz import process
-from discord.ext.commands.converter import RoleConverter
+from discord.ext.commands.converter import Converter
 from redbot.core import commands
 from redbot.core.commands import BadArgument
 from redbot.core.utils.chat_formatting import inline
 
-
-
-
-# original converter from https://github.com/TrustyJAID/Trusty-cogs/blob/master/serverstats/converters.py#L19
-
-class FuzzyRole(RoleConverter):
+class FuzzyItem(Converter):
     """
     This will accept role ID's, mentions, and perform a fuzzy search for
     roles within the guild and return a list of role objects
@@ -24,42 +15,42 @@ class FuzzyRole(RoleConverter):
     https://github.com/Rapptz/discord.py/blob/rewrite/discord/ext/commands/converter.py#L85
     https://github.com/Cog-Creators/Red-DiscordBot/blob/V3/develop/redbot/cogs/mod/mod.py#L24
     """
+    async def convert(self, ctx: commands.Context, argument: str) -> str:
+        items = [
+            "cursor",
+            "grandma",
+            "megaclicker",
+            "superclicker",
+            "epicclicker",
+            "factory",
+            "ultraclicker",
+            "godclicker",
+            "spamclicker",
+            "holyclicker",
+            "memeclicker",
+            "cookietrophy",
+            "blob",
+            "phencookie",
+            "flamecookie",
+            "flarecookie",
+            "aikacookie",
+            "trustycookie",
+            "kablekookie",
+            "neurocookie",
+            "yamicookie"
+        ]
 
-    def __init__(self, response: bool = True):
-        self.response = response
-        super().__init__()
-
-    async def convert(self, ctx: commands.Context, argument: str) -> discord.Role:
-        final_results = []
-        argument = argument.split(";;")
-        guild = ctx.guild
         result = []
-        for arg in argument:
-            if arg is None:
-                continue
-            arg = arg.lstrip("<@&").rstrip(">")
-            if arg.isdigit():
-                role = guild.get_role(int(arg))
-                if not role:
-                    pass 
-                else:
-                    if role in final_results:
-                        continue
-                    final_results.append(role)
-                    continue
-            for r in process.extract(
-                arg,
-                {r: unidecode(r.name) for r in guild.roles},
-                limit=None,
-                score_cutoff=75,
-            ):
-                result.append((r[2], r[1]))
+        for r in process.extract(
+            argument,
+            items,
+            limit=None,
+            score_cutoff=75,
+        ):
+            result.append(r[0])
 
-            
-            sorted_result = sorted(result, key=lambda r: r[1], reverse=True)
-            if sorted_result[0][0] in final_results:
-                continue
-            final_results.append(sorted_result[0][0])
-            result = []
-        
-        return final_results
+        if not result:
+            raise BadArgument(f'Shop "{argument}" not found.' if self.response else None)
+
+        sorted_result = sorted(result, key=lambda r: r[1], reverse=True)
+        return sorted_result[0]
