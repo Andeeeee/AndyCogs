c7ed6151599cd797b5cd10300a0fb631f873dff0
Andy <76832778+Andeeeee@users.noreply.github.com>
2021-02-15 18:17:25 -0800

    .

--- giveaways/giveaway.py
+++ giveaways/giveaway.py
@@ -177,7 +177,7 @@ class Giveaways(commands.Cog):
                 user_level = await mee6_api.get_user_rank(user.guild.id, user.id)
                 self.mee6_cache[str(user.guild.id)][str(user.id)] = user_level
             else:
-                user_level = await self.mee6_cache[str(user.guild.id)].get(str(user.id))
+                user_level = self.mee6_cache[str(user.guild.id)].get(str(user.id))
             choice = randint(1, 6)
             if choice == 3:
                 user_level = await mee6_api.get_user_rank(user.guild.id, user)
