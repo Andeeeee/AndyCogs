b8266ee02a67410697b759e77083fc629ab5dff8
Andy <76832778+Andeeeee@users.noreply.github.com>
2021-02-23 10:27:58 -0800

    parse cancer names

--- danklogs/danklogs.py
+++ danklogs/danklogs.py
@@ -362,7 +362,7 @@ class DankLogs(commands.Cog):
         if (await self.config.channel(message.channel).ignored()):
             return 
         last_message = await self.get_last_message(message)
-        filtered_content = message.content.strip().lstrip(f"<@{last_message.author.id}>").lstrip(f"<@!{last_message.author.id}>").replace("⏣ ", "").split()
+        filtered_content = message.content.strip().lstrip(f"<@{last_message.author.id}>").lstrip(f"<@!{last_message.author.id}>").replace("⏣ ", "").strip()
         filtered_content = "".join([unidecode(elem) for elem in filtered_content])
 
         match = re.match(gift_regex, filtered_content)
