aa3e356f8960cf1a9000c6f310f0697d0fa2e8b7
Andy <76832778+Andeeeee@users.noreply.github.com>
2021-02-23 16:41:13 -0800

    black style formatting

--- heist/__init__.py
+++ heist/__init__.py
@@ -1,4 +1,5 @@
 from .heist import Heist
 
+
 def setup(bot):
     bot.add_cog(Heist(bot))
--- heist/heist.py
+++ heist/heist.py
@@ -1,84 +1,87 @@
-import argparse 
+import argparse
 import asyncio
-import discord 
+import discord
 
 from rapidfuzz import process
-from redbot.core import commands, Config 
+from redbot.core import commands, Config
 from redbot.core.commands import Converter, BadArgument
 from redbot.core.utils.chat_formatting import humanize_list
 from typing import Optional
 from unidecode import unidecode
 
+
 async def heist_manager(ctx):
     if not ctx.guild:
         return False
-    if (await ctx.bot.is_owner(ctx.author)):
-        return True 
-    elif ctx.channel.permissions_for(ctx.author).administrator or ctx.channel.permissions_for(ctx.author).manage_guild:
-        return True 
+    if await ctx.bot.is_owner(ctx.author):
+        return True
+    elif (
+        ctx.channel.permissions_for(ctx.author).administrator
+        or ctx.channel.permissions_for(ctx.author).manage_guild
+    ):
+        return True
     else:
         cog = ctx.bot.get_cog("Heist")
         manager = await cog.config.guild(ctx.guild).manager()
         if not manager:
-            return False 
+            return False
         if manager not in [r.id for r in ctx.author.roles]:
-            return False 
+            return False
         return True
+
 
 class NoExitParser(argparse.ArgumentParser):
     def error(self, message):
         raise commands.BadArgument(message)
 
+
 class TimeConverter(Converter):
     async def convert(self, ctx: commands.Context, time: str) -> int:
         conversions = {"s": 1, "m": 60}
-        
+
         if str(time[-1]) not in conversions:
             if not str(time).isdigit():
                 raise BadArgument(f"{time} was not able to be converted to a time.")
-            return int(time) 
-        
+            return int(time)
+
         multiplier = conversions[str(time[-1])]
-        
+
         time = time[:-1]
         if not str(time).isdigit():
             raise BadArgument(f"{time} was not able to be converted to a time.")
-        
+
         return int(time) * multiplier
+
 
 class Heist(commands.Cog):
     def __init__(self, bot):
-        self.bot = bot 
+        self.bot = bot
         self.config = Config.get_conf(
             self,
             identifier=160805014090190130501014,
             force_registration=True,
         )
 
-        default_guild = {
-            "pingrole": None,
-            "sendembed": True,
-            "manager": None
-        }
+        default_guild = {"pingrole": None, "sendembed": True, "manager": None}
 
         self.config.register_guild(**default_guild)
-    
+
     def convert_amount(self, amount: str):
         conversions = {"k": 1000, "m": 1000000}
         if amount[-1] not in conversions:
             return round(int(amount))
         else:
             return round(int(amount[:-1]) * conversions[amount[-1]])
-    
+
     def display_time(self, seconds: int) -> str:
         message = ""
 
         intervals = (
-            ('week', 604_800),  # 60 * 60 * 24 * 7
-            ('day',   86_400),  # 60 * 60 * 24
-            ('hour',   3_600),  # 60 * 60
-            ('minute',    60),
-            ('second',     1),
+            ("week", 604_800),  # 60 * 60 * 24 * 7
+            ("day", 86_400),  # 60 * 60 * 24
+            ("hour", 3_600),  # 60 * 60
+            ("minute", 60),
+            ("second", 1),
         )
 
         for name, amount in intervals:
@@ -90,7 +93,7 @@ class Heist(commands.Cog):
             message += f'{n} {name + "s" * (n != 1)} '
 
         return message.strip()
-    
+
     def get_fuzzy_role(self, ctx, name: str):
         result = []
         for r in process.extract(
@@ -100,13 +103,13 @@ class Heist(commands.Cog):
             score_cutoff=75,
         ):
             result.append((r[2], r[1]))
-        
+
         if not result:
             raise BadArgument("{} is not a valid role")
 
         sorted_result = sorted(result, key=lambda r: r[1], reverse=True)
         return sorted_result[0][0]
-    
+
     async def get_heist_message(self, ctx, flags, sleep_time, early_time, role):
         heist_message = ""
         early_heist_message = ""
@@ -115,78 +118,88 @@ class Heist(commands.Cog):
             pingrole = await self.config.guild(ctx.guild).pingrole()
             role = ctx.guild.get_role(pingrole)
             heist_message += f"{role.mention}: "
-        
+
         if flags["early_roles"]:
             roles = humanize_list([r.name for r in flags["early_roles"]])
             early_heist_message += f"Channel Unlocked for **{roles}**! Unlocking in {self.display_time(early_time)} "
-        
+
         heist_message += f"Channel Unlocked for {role.name}! Locking in {self.display_time(sleep_time)} "
 
         return heist_message, early_heist_message
-    
+
     async def clean_flags(self, ctx, flags):
         if flags["donor"]:
             try:
                 donor = ctx.guild.get_member(int(flags["donor"]))
             except ValueError:
                 donor = discord.utils.get(ctx.guild.members, name=flags["donor"])
-            
+
             if not donor:
-                raise BadArgument("{} is not a valid member for the donor flag".format(flags["donor"]))
+                raise BadArgument(
+                    "{} is not a valid member for the donor flag".format(flags["donor"])
+                )
 
             flags["donor"] = donor
-        
+
         if flags["amt"]:
             try:
                 amt = self.convert_amount(flags["amt"])
             except ValueError:
-                raise BadArgument("{} was not able to be converted to a proper amount".format(flags["amt"]))
+                raise BadArgument(
+                    "{} was not able to be converted to a proper amount".format(
+                        flags["amt"]
+                    )
+                )
 
             flags["amt"] = amt
-        
+
         if flags["total"]:
             try:
                 total = self.convert_amount(flags["total"])
             except ValueError:
-                raise BadArgument("{} was not able to be converted to a proper amount".format(flags["total"]))
-        
+                raise BadArgument(
+                    "{} was not able to be converted to a proper amount".format(
+                        flags["total"]
+                    )
+                )
+
         if flags["early_roles"]:
             final_roles = []
             for r in flags["early_roles"]:
                 try:
                     role = ctx.guild.get_role(int(r))
                 except ValueError:
-                    role = self.get_fuzzy_role(r) 
+                    role = self.get_fuzzy_role(r)
                 final_roles.append(role)
-            flags["early_roles"] = final_roles 
-        
+            flags["early_roles"] = final_roles
+
         if flags["ping"]:
             pingrole = await self.config.guild(ctx.guild).pingrole()
             if not pingrole:
-                ping = False 
+                ping = False
             elif ctx.guild.get_role(pingrole) is None:
                 await self.config.guild(ctx.guild).pingrole.clear()
-                ping = False 
+                ping = False
             else:
-                ping = True 
+                ping = True
             flags["ping"] = ping
-            
+
         return flags
-    
+
     def get_sleep_time(self, long, early_roles, early_seconds):
         if long:
-            sleep_time = 240 
+            sleep_time = 240
         else:
             sleep_time = 90
         if not early_roles:
             return sleep_time, None
-            
+
         sleep_time -= early_seconds
         return sleep_time, early_seconds
-                
+
     @commands.group()
     async def heistset(self, ctx):
-        pass 
+        pass
 
     @heistset.command()
     async def manager(self, ctx, role: Optional[discord.Role] = None):
@@ -197,26 +210,28 @@ class Heist(commands.Cog):
         else:
             await self.config.guild(ctx.guild).manager.set(role.id)
             await ctx.send(f"**{role.name}** is now the manager role")
-    
+
     @heistset.command(aliases=["role"])
     @commands.admin_or_permissions(manage_guild=True)
     async def pingrole(self, ctx, role: Optional[discord.Role] = None):
         """Set the role to ping for heist start"""
         if not role:
             await self.config.guild(ctx.guild).pingrole.clear()
-            await ctx.send("Cleared your servers pingrole. I will no longer ping a role for heists")
+            await ctx.send(
+                "Cleared your servers pingrole. I will no longer ping a role for heists"
+            )
         else:
             await self.config.guild(ctx.guild).pingrole.set(role.id)
             await ctx.send(f"**{role.name}** will now be pinged for heists")
-    
+
     @commands.group()
     async def heist(self, ctx):
-        pass 
+        pass
 
     @heist.command(cooldown_after_parsing=True)
     @commands.guild_only()
     @commands.max_concurrency(1, commands.BucketType.channel)
-    @commands.cooldown(1, 30, commands.BucketType.channel) 
+    @commands.cooldown(1, 30, commands.BucketType.channel)
     @commands.check(heist_manager)
     @commands.bot_has_permissions(manage_channels=True, mention_everyone=True)
     async def start(
@@ -228,8 +243,10 @@ class Heist(commands.Cog):
         """Starts a heist"""
         if not unlockrole:
             unlockrole = ctx.guild.default_role
-            
-        allowed_mentions = discord.AllowedMentions(everyone=False, roles=True, users=True)
+
+        allowed_mentions = discord.AllowedMentions(
+            everyone=False, roles=True, users=True
+        )
 
         parser = NoExitParser()
         parser.add_argument("--long", action="store_true", default=False)
@@ -240,7 +257,7 @@ class Heist(commands.Cog):
         parser.add_argument("--total", default=None, nargs="?")
         parser.add_argument("--early-roles", default=None, nargs="*")
         parser.add_argument("--early-seconds", default=30, type=int, nargs="?")
-        
+
         if not flags:
             flags = {
                 "long": False,
@@ -257,69 +274,76 @@ class Heist(commands.Cog):
                 flags = vars(parser.parse_args(flags))
             except BadArgument as e:
                 return await ctx.send(str(e))
-            
+
             try:
                 args = await self.clean_flags(ctx, flags)
             except BadArgument as e:
                 return await ctx.send(str(e))
-        
-        sleep_time, early_time = self.get_sleep_time(flags["long"], flags["early_roles"], flags["early_seconds"])
-        heist_message, early_heist_message = await self.get_heist_message(ctx, flags, sleep_time, early_time, unlockrole)
+
+        sleep_time, early_time = self.get_sleep_time(
+            flags["long"], flags["early_roles"], flags["early_seconds"]
+        )
+        heist_message, early_heist_message = await self.get_heist_message(
+            ctx, flags, sleep_time, early_time, unlockrole
+        )
 
         emoji = self.bot.get_emoji(794006801019568138)
         if not emoji:
-            await ctx.send("Waiting for a heist message, send `CANCEL` to cancel the heist")
+            await ctx.send(
+                "Waiting for a heist message, send `CANCEL` to cancel the heist"
+            )
         else:
-            await ctx.send(f"Waiting for a heist message, send `CANCEL` to cancel the heist {emoji}")
-            
+            await ctx.send(
+                f"Waiting for a heist message, send `CANCEL` to cancel the heist {emoji}"
+            )
+
         def heist_check(m):
             if m.content == "CANCEL":
                 return True
             if not m.author.id == 270904126974590976:
-                return False 
+                return False
             if "They're trying to break into" not in m.content:
-                return False 
+                return False
             return True
 
         try:
             m = await self.bot.wait_for("message", check=heist_check, timeout=60)
         except asyncio.TimeoutError:
             return await ctx.send("Uh oh, you ran out of time. Try again later")
-        
+
         if m.content == "CANCEL":
             return await ctx.send("Cancelled the heist")
-        
 
         if flags["early_roles"]:
             for r in flags["early_roles"]:
                 overwrites = ctx.channel.overwrites_for(r)
-                overwrites.send_messages = True 
-                overwrites.read_messages = True 
+                overwrites.send_messages = True
+                overwrites.read_messages = True
                 await ctx.channel.set_permissions(r, overwrite=overwrites)
             await ctx.send(early_heist_message, allowed_mentions=allowed_mentions)
             await asyncio.sleep(early_time)
-        
+
         overwrites = ctx.channel.overwrites_for(unlockrole)
-        overwrites.send_messages = True 
-        overwrites.read_messages = True 
+        overwrites.send_messages = True
+        overwrites.read_messages = True
         await ctx.channel.set_permissions(unlockrole, overwrite=overwrites)
-        
+
         try:
             await ctx.send(heist_message, allowed_mentions=allowed_mentions)
         except discord.HTTPException:
-            pass 
+            pass
 
         await asyncio.sleep(sleep_time)
-        
+
         if flags["early_roles"]:
             for r in flags["early_roles"]:
                 overwrites = ctx.channel.overwrites_for(r)
                 overwrites.send_messages = False
                 overwrites.read_messages = False
                 await ctx.channel.set_permissions(r, overwrite=overwrites)
-        
+
         overwrites = ctx.channel.overwrites_for(unlockrole)
         overwrites.send_messages = False
         await ctx.channel.set_permissions(unlockrole, overwrite=overwrites)
 
-        await ctx.send("Times Up. Channel Locked")  
+        await ctx.send("Times Up. Channel Locked")
