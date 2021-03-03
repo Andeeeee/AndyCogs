0661e750b7f3a32e0241bcdbed523b48bc3545b5
Andy <76832778+Andeeeee@users.noreply.github.com>
2021-03-03 09:35:12 -0800

    decode cancer names

--- danklogs/danklogs.py
+++ danklogs/danklogs.py
@@ -14,7 +14,7 @@ from typing import Optional
 from unidecode import unidecode
 
 gift_regex = re.compile(
-    r"You gave (?P<user>.+\w)?  ?\*\*(?P<amount>[0-9,]+)\*\* ?(?:(?P<item>[a-zA-Z0-9_]{2,32}))?"
+    r"You gave (?P<user>.+\w)?  ?(?P<amount>[0-9,]+) ?(?:(?P<item>[a-zA-Z0-9_]{2,32}))?"
 )
 
 
