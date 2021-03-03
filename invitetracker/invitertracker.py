cb063ed612d018b29de1ebe8dfc584c8ee87da23
Andy <76832778+Andeeeee@users.noreply.github.com>
2021-03-02 16:13:30 -0800

    black style reformatting

--- invitetracker/invitertracker.py
+++ invitetracker/invitertracker.py
@@ -1,5 +1,5 @@
 import asyncio
-import discord 
+import discord
 
 from datetime import datetime
 from redbot.core import commands, Config
@@ -26,11 +26,10 @@ Sets your servers join/leave message. Variables:
 
 class InviteTracker(commands.Cog):
     """A cog for tracking who invited who"""
+
     def __init__(self, bot: Red):
-        self.bot = bot 
-        self.config = Config.get_conf(
-            self, 160805014090190130501014, True 
-        )
+        self.bot = bot
+        self.config = Config.get_conf(self, 160805014090190130501014, True)
 
         default_guild = {
             "invites": {},
@@ -49,20 +48,19 @@ class InviteTracker(commands.Cog):
         self.config.register_member(**default_member)
 
         self.invite_task = asyncio.create_task(self.invite_loop())
-    
 
     async def invite_loop(self):
         """Check every 5 minutes for updates to the invite links"""
         await self.bot.wait_until_ready()
         for guild_id, data in (await self.config.all_guilds()).items():
             if not data["enabled"]:
-                return 
+                return
             guild = self.bot.get_guild(int(guild_id))
             if not guild:
-                continue 
+                continue
             await self.save_invite_links(guild)
         await asyncio.sleep(300)
-    
+
     async def save_invite_links(self, guild: discord.Guild) -> bool:
         """Saves invite links to config"""
         invites = {}
@@ -83,14 +81,17 @@ class InviteTracker(commands.Cog):
                     "channel": channel.id,
                 }
             except Exception as e:
-                return False 
+                return False
         await self.config.guild(guild).invites.set(invites)
         return True
-    
+
     async def get_inviter(self, member: discord.Member) -> str:
         """Attempt to get the inviter of a user"""
         invites = await self.config.guild(member.guild).invites()
-        if member.guild.me.guild_permissions.manage_guild and "VANITY_URL" in member.guild.features:
+        if (
+            member.guild.me.guild_permissions.manage_guild
+            and "VANITY_URL" in member.guild.features
+        ):
             try:
                 link = str(await member.guild.vanity_invite())
             except (discord.errors.NotFound, discord.errors.HTTPException):
@@ -104,34 +105,37 @@ class InviteTracker(commands.Cog):
                         inviter = invite.inviter
 
         if not link:
-                for code, data in invites.items():
-                    try:
-                        invite = await self.bot.fetch_invite(code)
-                    except (
-                        discord.errors.NotFound,
-                        discord.errors.HTTPException,
-                    ):
-                        invite = None
-                    else:
-                        inviter = invite.inviter
+            for code, data in invites.items():
+                try:
+                    invite = await self.bot.fetch_invite(code)
+                except (
+                    discord.errors.NotFound,
+                    discord.errors.HTTPException,
+                ):
+                    invite = None
+                else:
+                    inviter = invite.inviter
 
-                    if not invite:
-                        if (data["max_uses"] - data["uses"]) == 1:
-                            try:
-                                inviter = await self.bot.fetch_user(data["inviter"])
-                            except (discord.errors.NotFound, discord.errors.Forbidden):
-                                inviter = None
+                if not invite:
+                    if (data["max_uses"] - data["uses"]) == 1:
+                        try:
+                            inviter = await self.bot.fetch_user(data["inviter"])
+                        except (discord.errors.NotFound, discord.errors.Forbidden):
+                            inviter = None
 
         await self.save_invite_links(member.guild)
 
-        if (member.guild.me.guild_permissions.manage_guild and member.guild.me.guild_permissions.view_audit_log) and not link:
+        if (
+            member.guild.me.guild_permissions.manage_guild
+            and member.guild.me.guild_permissions.view_audit_log
+        ) and not link:
             valid_action = discord.AuditLogAction.invite_create
             async for log in member.guild.audit_logs(action=valid_action):
                 if log.target.code not in invites:
                     inviter = log.target.inviter
                     break
         return inviter
-    
+
     def cog_unload(self):
         self._unload()
 
@@ -142,104 +146,120 @@ class InviteTracker(commands.Cog):
     @commands.group()
     async def invitetrackerset(self, ctx):
         """Set server settings for invite tracking"""
-        pass 
+        pass
 
     @invitetrackerset.command()
     @commands.admin_or_permissions(manage_guild=True)
     async def joinchannel(self, ctx, channel: Optional[discord.TextChannel] = None):
+        """Sets the channel join messages are sent to"""
         if not channel:
             await self.config.guild(ctx.guild).joinchannel.clear()
             await ctx.send("No longer sending invite messages to a channel")
         else:
             await self.config.guild(ctx.guild).joinchannel.set(channel.id)
             await ctx.send(f"Now logging member invite messages to {channel.mention}")
-    
+
     @invitetrackerset.command(aliases=["joinmsg"], usage=INVITE_MESSAGE_USAGE)
     @commands.admin_or_permissions(manage_guild=True)
-    async def joinmessage(self, ctx, *, message = None):
+    async def joinmessage(self, ctx, *, message=None):
+        """Set the message when a user joins the server"""
         if not message:
             await self.config.guild(ctx.guild).joinmessage.clear()
             await ctx.send("The message has been reset")
         else:
             await self.config.guild(ctx.guild).joinmessage.set(message)
             await ctx.send("The message has been set")
-    
+
     @invitetrackerset.command()
     @commands.admin_or_permissions(manage_guild=True)
     async def leavechannel(self, ctx, channel: Optional[discord.TextChannel] = None):
+        """Sets the channel leave messages are sent to"""
         if not channel:
             await self.config.guild(ctx.guild).leavechannel.clear()
             await ctx.send("No longer sending invite messages to a channel")
         else:
             await self.config.guild(ctx.guild).leavechannel.set(channel.id)
             await ctx.send(f"Now logging member invite messages to {channel.mention}")
-    
+
     @invitetrackerset.command(aliases=["leavemsg"], usage=INVITE_MESSAGE_USAGE)
     @commands.admin_or_permissions(manage_guild=True)
-    async def leavemessage(self, ctx, *, message = None):
+    async def leavemessage(self, ctx, *, message=None):
+        """Set the message when a user leaves the server"""
         if not message:
             await self.config.guild(ctx.guild).leavemessage.clear()
             await ctx.send("The message has been reset")
         else:
             await self.config.guild(ctx.guild).leavemessage.set(message)
             await ctx.send("The message has been set")
-    
+
     @invitetrackerset.command(aliases=["showsettings"])
     async def settings(self, ctx):
+        """View server settings for invite tracking"""
         data = await self.config.guild(ctx.guild).all()
-        e = discord.Embed(title=f"Invite Settings for {ctx.guild.name}", color = await ctx.embed_color())
+        e = discord.Embed(
+            title=f"Invite Settings for {ctx.guild.name}", color=await ctx.embed_color()
+        )
         e.add_field(name="Join Channel", value=f"<#{data['joinchannel']}")
         e.add_field(name="Leave Channel", value=f"<#{data['leavechannel']}")
         e.add_field(name="Join Message", value=box(data["joinmessage"]), inline=False)
         e.add_field(name="Leave Message", value=box(data["leavemessage"]), inline=False)
         await ctx.send(embed=e)
-        
-    
 
-    
     @commands.group(invoke_without_command=True)
     async def invites(self, ctx, user: Optional[discord.Member] = None):
         """View your own/ a users invites in this server"""
         if not ctx.invoked_subcommand:
             if not user:
-                user = ctx.author 
+                user = ctx.author
             invites = await self.config.member(user).invites()
             await ctx.send(f"**{user.name}** has **{invites}** invites")
-    
+
     @invites.command()
     async def who(self, ctx, user: Optional[discord.Member] = None):
         """View the person that invited you or another member"""
         if not user:
-            user = ctx.author 
+            user = ctx.author
         inviter = await self.config.member(user).inviter()
         if not inviter:
-            return await ctx.send(f"**{user}** either joined from a vanity invite or I couldn't track this")
+            return await ctx.send(
+                f"**{user}** either joined from a vanity invite or I couldn't track this"
+            )
         elif ctx.guild.get_member(inviter) is None:
-            return await ctx.send(f"**{user}**'s inviter seems to have left the server, the id is {inviter}")
+            return await ctx.send(
+                f"**{user}**'s inviter seems to have left the server, the id is {inviter}"
+            )
         else:
             inviter = ctx.guild.get_member(inviter)
             await ctx.send(f"**<@{inviter.display_name}>** invited **{user}**")
-    
+
     @invites.command()
-    async def top(self, ctx, amount: Optional[int] = 10, top_to_bottom: Optional[bool] = True):
+    async def top(
+        self, ctx, amount: Optional[int] = 10, top_to_bottom: Optional[bool] = True
+    ):
         """View the top/bottom inviters"""
         all_members = await self.config.all_members(ctx.guild)
-        member_data = [(member, data["invites"]) for member, data in all_members if data["count"] > 0 and ctx.guild.get_member(int(member)) is not None]
-        sorted_data = sorted(member_data, key = lambda k: k[1], reverse=top_to_bottom)[:amount]
+        member_data = [
+            (member, data["invites"])
+            for member, data in all_members
+            if data["count"] > 0 and ctx.guild.get_member(int(member)) is not None
+        ]
+        sorted_data = sorted(member_data, key=lambda k: k[1], reverse=top_to_bottom)[
+            :amount
+        ]
 
         leaderboard = ""
 
         for i, member, invites in enumerate(sorted_data, start=1):
             leaderboard += f"{i}. <@{member}>: {invites}"
-        
+
         if len(leaderboard) >= 2048:
             pages = list(pagify(leaderboard))
             embeds = []
             for i, page in enumerate(pages, start=1):
                 e = discord.Embed(
-                    title = f"Invite Leaderboard for {ctx.guild.name}",
-                    color = await ctx.embed_color(),
-                    description = page,
+                    title=f"Invite Leaderboard for {ctx.guild.name}",
+                    color=await ctx.embed_color(),
+                    description=page,
                 )
                 e.set_author(name=ctx.guild.name, icon_url=ctx.guild.icon_url)
                 e.set_footer(text=f"Page {i}/{len(pages)} pages")
@@ -247,14 +267,13 @@ class InviteTracker(commands.Cog):
             await menu(ctx, embeds, DEFAULT_CONTROLS)
         else:
             e = discord.Embed(
-                title = f"Invite Leaderboard for {ctx.guild.name}",
-                color = await ctx.embed_color(),
-                description = leaderboard
+                title=f"Invite Leaderboard for {ctx.guild.name}",
+                color=await ctx.embed_color(),
+                description=leaderboard,
             )
             e.set_author(name=ctx.guild.name, icon_url=ctx.guild.icon_url)
             await ctx.send(embed=e)
-        
-    
+
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
         guild = member.guild
@@ -269,16 +288,18 @@ class InviteTracker(commands.Cog):
             await self.config.guild(member.guild).joinchannel.clear()
         if not inviter:
             if not channel:
-                return 
+                return
             else:
-                await channel.send(f"I couldn't figure out how **{member.display_name}** joined")
+                await channel.send(
+                    f"I couldn't figure out how **{member.display_name}** joined"
+                )
         else:
             await self.config.member(member).inviter.set(inviter.id)
             async with self.config.member(inviter).invites() as invites:
                 invites += 1
                 inviter_invites = invites
             if not channel:
-                return 
+                return
             message = await self.config.guild(member.guild).joinmessage()
             replace_dict = {
                 "{inviter}": inviter.mention,
@@ -298,7 +319,7 @@ class InviteTracker(commands.Cog):
             for word, replacement in replace_dict.items():
                 message.replace(word, replacement)
             await channel.send(message)
-    
+
     @commands.Cog.listener()
     async def on_member_leave(self, member: discord.Member):
         guild = member.guild
@@ -313,15 +334,19 @@ class InviteTracker(commands.Cog):
             await self.config.guild(guild).leavechannel.clear()
         if not inviter:
             if not channel:
-                return 
+                return
             else:
-                await channel.send(f"I couldn't figure out who inivited **{member.name}**")
+                await channel.send(
+                    f"I couldn't figure out who inivited **{member.name}**"
+                )
         else:
-            async with self.config.member_from_ids(member.guild.id, inviter).invites() as invites:
+            async with self.config.member_from_ids(
+                member.guild.id, inviter
+            ).invites() as invites:
                 invites -= 1
                 inviter_invites = invites
             if not channel:
-                return 
+                return
             message = await self.config.guild(member.guild).joinmessage()
             replace_dict = {
                 "{inviter}": inviter.mention,
@@ -341,6 +366,3 @@ class InviteTracker(commands.Cog):
             for word, replacement in replace_dict.items():
                 message.replace(word, replacement)
             await channel.send(message)
-
-        
-
