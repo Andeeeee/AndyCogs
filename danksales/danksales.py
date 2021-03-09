79e2e3dbd1a4087263b90e04e747c98bc9509dfc
Andy <76832778+Andeeeee@users.noreply.github.com>
2021-03-09 09:38:01 -0800

    attempt to parse noumenon/kronos shop sale msgs

--- danksales/danksales.py
+++ danksales/danksales.py
@@ -75,7 +75,7 @@ class DankSales(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message_without_command(self, message: discord.Message):
-        if not message.author.id == 270904126974590976:
+        if not message.author.id == 270904126974590976 or message.webhook_id is None:
             return "not dank"
         if not message.embeds:
             return "no embeds"
